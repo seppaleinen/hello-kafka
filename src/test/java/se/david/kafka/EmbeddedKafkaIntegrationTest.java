bfc1f30e330500752cc8cead27e23715446b9084
David Eriksson <davideriksson@swedenmail.com>
2017-07-05 22:09:55 +0200

    Simple kafka consumer/producer application with embedded kafka test

---
+++ src/test/java/se/david/kafka/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,75 @@
+package se.david.kafka;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.listener.MessageListenerContainer;
+import org.springframework.kafka.test.rule.KafkaEmbedded;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.context.junit4.SpringRunner;
+import se.david.kafka.consumer.Receiver;
+import se.david.kafka.producer.Sender;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmbeddedKafkaIntegrationTest {
+    private static final String SENDER_TOPIC = "sender.t";
+    private static final String RECEIVER_TOPIC = "receiver.t";
+
+    @Autowired
+    private Sender sender;
+
+    @Autowired
+    private Receiver receiver;
+
+    @Autowired
+    private KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+    @ClassRule
+    public static KafkaEmbedded embeddedKafka = new KafkaEmbedded(
+            1,
+            true,
+            SENDER_TOPIC,
+            RECEIVER_TOPIC
+    );
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        String kafkaBootstrapServers = embeddedKafka.getBrokersAsString();
+
+        // override the property in application.properties
+        System.setProperty("kafka.bootstrap-servers", kafkaBootstrapServers);
+    }
+
+
+    @Before
+    public void setUp() throws Exception {
+        // wait until the partitions are assigned
+        for (MessageListenerContainer messageListenerContainer : kafkaListenerEndpointRegistry
+                .getListenerContainers()) {
+            ContainerTestUtils.waitForAssignment(
+                    messageListenerContainer,
+                    embeddedKafka.getPartitionsPerTopic()
+            );
+        }
+    }
+
+    @Test
+    public void testReceive() throws Exception {
+        sender.send(RECEIVER_TOPIC, "Hello Spring Kafka!");
+
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        // check that the message was received
+        assertThat(receiver.getLatch().getCount()).isEqualTo(0);
+    }
+}
